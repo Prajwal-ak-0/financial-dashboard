81961e8c3b543bdb70355cea7ab1425e1f03a805
Prajwal <prajwalhassan7@gmail.com>
2023-08-12 16:24:37 +0530

    Done with new route

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Userform } from './components/Userform'
 
 const page = () => {
   return (
-    <div>kwdgfekrjf kejrgher </div>
+    <div className='flex-col mt-3'>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Userform/>
+      </div>
+    </div>
   )
 }
 
--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -4,12 +4,14 @@ import { useCallback, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
 import { buttonVariants } from "@/components/ui/button"
+import MenuItem from "./MenuItem";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const UserMenu = ({}) => {
   const router = useRouter();
 
   const [isOpen, setIsOpen] = useState(false);
-
+  
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
   }, []);
@@ -17,21 +19,74 @@ const UserMenu = ({}) => {
   return ( 
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
-        <Button 
-            onClick={() => router.push('/new')}
-            className=
-            {buttonVariants({variant:"ghost"})}   
+        <div className="md:flex gap-4 hidden ">
+          <Button 
+              onClick={() => router.push('/new')}
+              className=
+              {buttonVariants({variant:"ghost"})}   
+          >
+              Add User
+          </Button>
+          <Button 
+              onClick={() => router.push('/dashboard')}
+          className=
+              {buttonVariants({variant:"ghost"})}    
+          >
+              Dashboard
+          </Button>
+        </div>
+        <div 
+        onClick={toggleOpen}
+        className="
+          p-4
+          py-2
+          px-2
+          ml-6
+          border-[1px] 
+          border-neutral-200 
+          flex 
+          flex-row 
+          items-center 
+          gap-3 
+          rounded-full 
+          cursor-pointer 
+          hover:shadow-md
+          transition
+          "
         >
-            Add User
-        </Button>
-        <Button 
-            onClick={() => router.push('/dashboard')}
-        className=
-            {buttonVariants({variant:"ghost"})}    
-        >
-            Dashboard
-        </Button>
+          <AiOutlineMenu size={25} />
+        </div>
       </div>
+      {isOpen && (
+        <div 
+          className="
+            absolute 
+            rounded-xl 
+            shadow-md
+            w-[40vw]
+            md:w-3/4 
+            bg-white
+            overflow-hidden 
+            top-12 
+            text-[15px]
+            ml-[75px]
+            border-black
+          "
+        >
+          <div className="flex flex-col cursor-pointer">
+              <>
+                <MenuItem 
+                  label="Add User"
+                  onClick={() => router.push('/new')}
+                />
+                <MenuItem 
+                  label="Dashboard" 
+                  onClick={() => router.push('/dashboard')}
+                />
+              </>
+          </div>
+        </div>
+      )}
     </div>
    );
 }
